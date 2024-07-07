61ff1746a7f294c467267cc0b1e9d3df9c8d3a50
soumyanavaneeth <soumyanavaneeth03@gmail.com>
2024-07-07 12:36:41 +0530

    Commit

--- src/ex_07072024/Lab001.java
+++ src/ex_07072024/Lab001.java
@@ -1,6 +1,6 @@
 package ex_07072024;
 
-public class Lab_001 {
+public class Lab001 {
     public static void main(String[] args) {
         System.out.println("Hello Soumya");
     }
--- src/ex_07072024/Lab002.java
+++ src/ex_07072024/Lab002.java
@@ -1,4 +1,9 @@
 package ex_07072024;
 
 public class Lab002 {
+    public static void main(String[] args) {
+        System.out.println(2+2);
+        //Comment -> Single Line
+        /* Multiline */
+    }
 }
