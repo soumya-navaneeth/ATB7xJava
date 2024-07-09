002dd35204a669783a136274c67dd31611bd0380
soumyanavaneeth <soumyanavaneeth03@gmail.com>
2024-07-09 16:47:01 +0530

    Updated

--- src/ex_07072024/Lab001_HelloWorld.java
+++ src/ex_07072024/Lab001_HelloWorld.java
@@ -1,6 +1,6 @@
 package ex_07072024;
 
-public class Lab001 {
+public class Lab001_HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello Soumya");
     }
---
+++ src/ex_07072024/Lab003_MathOperators.java
@@ -0,0 +1,13 @@
+package ex_07072024;
+
+public class Lab003_MathOperators {
+    public static void main(String[] args) {
+        System.out.println("This is to test Math Operator");
+        System.out.println(2+2);
+        System.out.println(8-2);
+        System.out.println(8*2);
+        System.out.println(66/2);
+        System.out.println(5/2); // Prints only Integer
+
+    }
+}
