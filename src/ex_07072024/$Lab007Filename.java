e5ec7baf0d6ce7fd8ed24c5b5334a8e8b75ea0fa
soumyanavaneeth <soumyanavaneeth03@gmail.com>
2024-07-09 18:09:06 +0530

    Updated

---
+++ src/ex_07072024/$Lab007Filename.java
@@ -0,0 +1,7 @@
+package ex_07072024;
+
+public class $Lab007Filename {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/ex_07072024/Lab006_MultipleMain.java
@@ -0,0 +1,11 @@
+package ex_07072024;
+
+public class Lab006_MultipleMain {
+    public static void main(String[] args) {
+        System.out.println("This is the 1st main text");
+    }
+
+    public static void main(String[] args) {
+        System.out.println("this is 2nd main text");
+    }
+}
