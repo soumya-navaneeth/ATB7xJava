4b198f2df88b5dde12c0d9b53be9800db5c26ed3
soumyanavaneeth <soumyanavaneeth03@gmail.com>
2024-07-21 22:20:49 +0530

    Added new

--- src/ex_07072024/Lab006_MultipleMain.java
+++ src/ex_07072024/Lab006_MultipleMain.java
@@ -4,8 +4,8 @@ public class Lab006_MultipleMain {
     public static void main(String[] args) {
         System.out.println("This is the 1st main text");
     }
-
-    public static void main(String[] args) {
-        System.out.println("this is 2nd main text");
-    }
 }
+
+//    public static void main(String[] args) {
+//        System.out.println("this is 2nd main text");
+//        }
