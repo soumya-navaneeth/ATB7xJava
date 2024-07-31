dc007e6533b1012ab4744a3a6f6d909ef9efef2c
soumyanavaneeth <soumyanavaneeth03@gmail.com>
2024-07-31 22:43:02 +0530

    Added new

--- src/ex_21072024/Lab081_LoopBreakContinue.java
+++ src/ex_21072024/Lab081_LoopBreakContinue.java
@@ -1,6 +1,6 @@
 package ex_21072024;
 
-public class Lab081_LoopBreak {
+public class Lab081_LoopBreakContinue {
     public static void main(String[] args) {
         // Loop with condition
         for (int i = 1; i <= 10; i++) {
@@ -9,6 +9,13 @@ public class Lab081_LoopBreak {
                 break; // due to this break statement, even if condition says tll 10, it will exit
             }
 
+        }
+        for (int i = 0; i < 10; i++) { // i = 0 to 9, times  = 10
+            System.out.println(i);
+            if (i == 5) {
+                continue; // this continues even when condition is matched
+            }
+            System.out.println("After!!");
         }
     }
 }
--- src/ex_21072024/Lab082_PrintEvenOddNumbers.java
+++ src/ex_21072024/Lab082_PrintEvenOddNumbers.java
@@ -8,6 +8,13 @@ public class Lab082_PrintEvenOddNumbers {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the upper limit");
         int num = sc.nextInt();
+//        for (int i = 0; i <= num; i++) { // i = 0 to 50, times  = 51
+//            if (i % 2 == 0) {
+//                System.out.println("Even -> " + i);
+//                continue;
+//            }
+//            System.out.println("Odd -> " + i);
+//        }
 //        Print even numbers
         System.out.println("Even Numbers are:");
         for (int i = 0; i <=num ; i++) {
